e99ec089e074806e1e46a01d1706fe0243d03ddd
Training_B4C.05.15 <Training_B4C.05.15@BDC7-D-1SH2N62.dir.svc.accenture.com>
2019-08-14 10:19:28 +0530

    push to github

---
+++ javaDemo/src/pack1/calculator.java
@@ -0,0 +1,12 @@
+package pack1;
+
+public class calculator {
+	int empNo;//class variable or instance variable
+	public static void main(String[] args) {
+		int mark1=10;//local variable
+		int mark2=20;
+		int totalmark=mark1+mark2;
+		System.out.println("The total mark:"+totalmark);
+	}
+
+}
